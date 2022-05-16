52a9697b30af0412f4656cfaa8cc3b8c89d40ce6
Alexa-Iordache <alexa.iordache2013@gmail.com>
2022-05-16 19:32:03 +0300

    3 images added on coffee page

--- src/coffee-page/index.jsx
+++ src/coffee-page/index.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import './coffee-page.css';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
+import { IconButton, ImageListItemBar } from '@mui/material';
+import { GoInfo } from 'react-icons/go';
+import image1 from './images/espresso_scurt.png';
+import image2 from './images/espresso_lung.png';
+import image3 from './images/americano.png';
 
 // function to calculate the proper size of an image
 // the 'srcset' attribute allows me to specify a list of images along with dimension I want the images
@@ -10,7 +15,7 @@ import ImageListItem from '@mui/material/ImageListItem';
 
 function srcset(image, size, rows = 1, cols = 1) {
   return {
-    // src: `${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format`,
+    src: `${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format`,
     srcSet: `${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format&dpr=2 2x`,
   };
 }
@@ -32,6 +37,18 @@ export default function CoffeePage() {
                 alt={item.title}
                 loading="lazy"
               />
+            <ImageListItemBar
+                title={item.title}
+                actionIcon={
+                  <IconButton
+                    sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
+                    aria-label={`info about ${item.title}`}
+                    onClick={() => { console.log('onClick'); }} // when the info button is pressed -> a modal with the info should appear
+                  >
+                      <GoInfo/>
+                  </IconButton>
+                }
+            />
             </ImageListItem>
           ))}
         </ImageList>
@@ -41,62 +58,21 @@ export default function CoffeePage() {
 }
 
 const itemData = [
+
   {
-    img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
-    title: 'Breakfast',
+    img: image1,
+    title: 'Espresso scurt',
     rows: 2,  // the first image I want to occupy 2 rows and 2 columns
     cols: 2,
   },
   {
-    img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
-    title: 'Burger',  // this image i want to be a normal image, so it will occupy 1 row and 1 column 
+    img: image2,
+    title: 'Espresso lung',  // this image will occupy 1 row and 2 columns 
+    cols: 2
   },
   {
-    img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
-    title: 'Camera',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
-    title: 'Coffee',
+    img: image3,
+    title: 'Americano',
     cols: 2,
   },
-  //   {
-  //     img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
-  //     title: 'Hats',
-  //     cols: 2,
-  //   },
-  //   {
-  //     img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
-  //     title: 'Honey',
-  //     author: '@arwinneil',
-  //     rows: 2,
-  //     cols: 2,
-  //   },
-  //   {
-  //     img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
-  //     title: 'Basketball',
-  //   },
-  //   {
-  //     img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
-  //     title: 'Fern',
-  //   },
-  //   {
-  //     img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
-  //     title: 'Mushrooms',
-  //     rows: 2,
-  //     cols: 2,
-  //   },
-  //   {
-  //     img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
-  //     title: 'Tomato basil',
-  //   },
-  //   {
-  //     img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
-  //     title: 'Sea star',
-  //   },
-  //   {
-  //     img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
-  //     title: 'Bike',
-  //     cols: 2,
-  //   },
 ];
