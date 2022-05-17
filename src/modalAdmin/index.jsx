871b21d3d1fe3b5a403324d0ef6f32d38d839b98
Alexa-Iordache <alexa.iordache2013@gmail.com>
2022-05-17 13:51:27 +0300

    login button added

--- src/modalAdmin/index.jsx
+++ src/modalAdmin/index.jsx
@@ -1,9 +1,31 @@
-import React from "react";
-import { Modal, Box, Typography, TextField } from '@mui/material';
+import React, { useState } from "react";
+import { Modal, Box, Typography, TextField, Button } from '@mui/material';
 
 export default function ModalAdmin(props) {
     const open = props.open;
     const handleClose = props.handleClose;
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    // function that updates the text from tge 'username' input 
+    const handleUsername = (event) => {
+        setUsername(event.target.value);
+        // console.log(username);
+    }
+
+    // function that updates the text from tge 'password' input 
+    const handlePassword = (event) => {
+        setPassword(event.target.value);
+    }
+
+    const handleClick = (username, password) => {
+        {(username === 'admin' && password === 'admin')
+        ? console.log("admis")
+        : console.log("neadmis")
+        }
+    }
+
 
     return (
         <Modal
@@ -25,17 +47,37 @@ export default function ModalAdmin(props) {
                         id="outlined-required"
                         label="Required"
                         defaultValue=" Username"
+                        value={username}
+                        onChange={() => {handleUsername()}}
                     />
                 </Typography>
 
                 <Typography sx={{ marginTop: 3 }} >
-                <TextField
+                    <TextField
                         required
                         id="outlined-required"
                         label="Required"
                         defaultValue=" Password"
+                        value={password}
+                        onChange={() => {handlePassword()}}
                     />
                 </Typography>
+
+                <Typography sx={styleButton} >
+                    <Button
+                        onClick={() => {handleClick(username, password)}} // when the user clickes the 'login' button, 
+                        // they are redirected to the 'dashboard' page
+                        disabled={username === '' || password === '' ? true : false} // if username or password is not written, 
+                        // then the user can not log in (the 'login' button is disabled) 
+
+                        // the color of the button will be grey if it is disabled, and it will turn blue when it is abled
+                        style={{
+                            backgroundColor: username === '' || password === '' ? 'grey' : 'white',
+                            borderColor: username === '' || password === '' ? 'grey' : 'white'
+                        }}>
+                        Login </Button>
+                </Typography>
+
 
             </Box>
         </Modal>
@@ -53,4 +95,12 @@ const styleModal = {
     border: '5px double #000',
     boxShadow: 24,
     p: 4,
+};
+
+
+const styleButton = {
+    display: 'flex',
+    justifyContent: 'center',
+    marginTop: '15px',
+    paddingTop: '10px',
 };
